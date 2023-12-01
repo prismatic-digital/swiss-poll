3f4d1b331a773de5b7cbe44b7a9ca1f2e345a2aa
Mohamed Amine Dakak <amine.dakak@gmail.com>
2023-12-01 15:36:32 +0100

    fix

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,4 +1,4 @@
-import { NextApiRequest, NextApiResponse } from "next";
+// import { NextApiRequest, NextApiResponse } from "next";
 // import getConfig from "next/config";
 import NextAuth from "next-auth";
 import type { NextAuthOptions } from 'next-auth'
