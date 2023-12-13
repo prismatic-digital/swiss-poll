98d3e01a9f8e24424ae4052e6be08ae37c69def7
Mohamed Amine Dakak <amine.dakak@gmail.com>
2023-12-13 15:32:34 +0100

    update default values

---
+++ prisma/migrations/20231213143153_update_default_values_for_preferences_fields/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE "NoCodeForm" ALTER COLUMN "backgroundColor" SET DEFAULT '#ffffff',
+ALTER COLUMN "buttonsColor" SET DEFAULT '#334155',
+ALTER COLUMN "textColor" SET DEFAULT '#334155';
